f0abf4ff6897f1ecc1e08d7e46d54edddc950f13
gus98 <gustavo.silvapes2020@gmail.com>
2024-05-27 20:51:03 -0300

    Feature: Using populate

--- src/infrastructure/user/repository/mongodb/UserRepositoryMondodb.ts
+++ src/infrastructure/user/repository/mongodb/UserRepositoryMondodb.ts
@@ -1,11 +1,7 @@
-import dotenv from "dotenv";
-
 import type UserRepositoryInterface from "../../../../domain/user/repository/UserRepositoryInterface";
 import UserEntity from "../../../../domain/user/entity/UserEntity";
 
 import RepositoryMongoDb from "../../../mongodb/RepositoryMongodb";
-
-dotenv.config();
 
 export default class UserRepositoryMongoDb implements UserRepositoryInterface {
   private repository: RepositoryMongoDb;
@@ -20,24 +16,37 @@ export default class UserRepositoryMongoDb implements UserRepositoryInterface {
   public async findAll() {
     const users = await this.repository.findAll();
 
-    return users.map(({ _id, name, login, password, createdAt, updatedAt }) => {
-        const user = new UserEntity(name, login, password);
-        user.changeId(_id.toString());
-        user.changeCreatedAt(createdAt);
-        user.changeUpdatedAt(updatedAt);
+    if (!users) {
+      throw new Error("Users not found");
+    }
 
-        return user;
-    });
+    return users.map(({ _id, name, login, password, createdAt, updatedAt }) =>
+      UserEntity.populate({ 
+        id: _id.toString(), 
+        name, 
+        login, 
+        password, 
+        createdAt, 
+        updatedAt 
+      })
+    );
   }
 
   public async findById(id: string): Promise<UserEntity> {
     const user = await this.repository.findById(id);
 
     if (!user) {
-        throw new Error("User not found");
+      throw new Error("User not found");
     }
 
-    return new UserEntity(user.name, user.login, user.password);
+    return UserEntity.populate({ 
+      id: user._id.toString(), 
+      name: user.name, 
+      login: user.login, 
+      password: user.password, 
+      createdAt: user.createdAt, 
+      updatedAt: user.updatedAt 
+    });
   }
 
   public async update(entity: UserEntity): Promise<void> {
